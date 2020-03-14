c85d7aeceae58989b44488fe83c263f2dbd70427
simmontali <sim.montali@gmail.com>
2020-03-14 22:33:30 +0100

    Added response codes

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -15,15 +15,22 @@ exports.getUserID = function(req, res) {
 }
 exports.signUp = function(req, res) {
     bcrypt.hash(req.body.password, 10, function(err, hash) {
+        User.findOne({username: req.params.username}, function (err, user){
+            if (user.username) {
+                res.status(409).status()
+            }
+        }
         // Store user in database
         var new_user = new User({
             username: req.body.username,
             password: hash
         });
         new_user.save(function(err, user) {
-            if (err)
-                res.send(err);
-            res.send(user.username);
+            if (err){
+                res.status(401).send(err);
+            } else {
+            res.status(201).send(user.username);
+            }
         });
       });
 }
@@ -40,7 +47,7 @@ exports.login = function(req, res) {
                  res.send(req.params.username);
                 } else {
                  // Passwords don't match
-                 res.send();
+                 res.status(401).send();
                 } 
               });
     });
@@ -57,7 +64,7 @@ exports.logout = function(req, res) {
                  res.send(req.params.username);
                 } else {
                  // Passwords don't match
-                 res.send();
+                 res.status(401).send();
                 } 
               });
     });
--- server.js
+++ server.js
@@ -12,7 +12,11 @@ mongoose.connect('mongodb://localhost/userdb');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 var routes = require('./api/routes/userRoutes'); //importing route
 routes(app); //register the route
