8bef705f0e52b36e47d917ab75ba3c7f31188b61
simmontali <sim.montali@gmail.com>
2020-03-15 12:05:32 +0100

    Fix logout, CORS, response codes.

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,73 +1,73 @@
-'use strict';
+"use strict";
 
-
-const bcrypt = require('bcrypt');
-var mongoose = require('mongoose'),
-  User = mongoose.model('Users');
-
+const bcrypt = require("bcrypt");
+var mongoose = require("mongoose"),
+  User = mongoose.model("Users");
 
 exports.getUserID = function(req, res) {
-    User.findOne({username: req.params.username}, function (err, user){
-    if (err)
-        res.send(err);
-    res.send(user.connectionID);
-    });
-}
+  User.findOne({ username: req.params.username }, function(err, user) {
+    if (err) res.send(err);
+    if (typeof user.connectionID !== undefined) res.send(user.connectionID);
+    else res.status(404).send();
+  });
+};
 exports.signUp = function(req, res) {
-    bcrypt.hash(req.body.password, 10, function(err, hash) {
-        User.findOne({username: req.params.username}, function (err, user){
-            if (user.username) {
-                res.status(409).status()
-            }
-        }
-        // Store user in database
-        var new_user = new User({
-            username: req.body.username,
-            password: hash
-        });
-        new_user.save(function(err, user) {
-            if (err){
-                res.status(401).send(err);
-            } else {
-            res.status(201).send(user.username);
-            }
-        });
+  bcrypt.hash(req.body.password, 10, function(err, hash) {
+    User.findOne({ username: req.params.username }, function(err, user) {
+      if (user.hasOwn("username")) {
+        res.status(409).status();
+        return;
+      }
+      // Store user in database
+      const new_user = new User({
+        username: req.body.username,
+        password: hash
       });
-}
+      new_user.save(function(err, user) {
+        if (err) {
+          res.status(401).send(err);
+        } else {
+          res.status(201).send(user.username);
+        }
+      });
+    });
+  });
+};
 
 exports.login = function(req, res) {
-    User.findOne({username: req.params.username}, function(err, user) {
-        if(err)
-            res.send(err);
-        bcrypt.compare(req.body.password, user.password, function(err, hashRes) {
-                if(hashRes) {
-                 // Passwords match
-                 user.connectionID = req.body.connectionID;
-                 user.save();
-                 res.send(req.params.username);
-                } else {
-                 // Passwords don't match
-                 res.status(401).send();
-                } 
-              });
-    });
-}
+  User.findOne({ username: req.params.username }, function(err, user) {
+    if (err || user == null) res.status(401).send(err);
+    else {
+      bcrypt.compare(req.body.password, user.password, function(
+        cryptErr,
+        hashRes
+      ) {
+        if (hashRes) {
+          // Passwords match
+          user.connectionID = req.body.connectionID;
+          user.save();
+          res.send(req.params.username);
+        } else if (cryptErr) {
+          // Passwords don't match
+          res.status(401).send();
+        }
+      });
+    }
+  });
+};
 exports.logout = function(req, res) {
-    User.findOne({username: req.params.username}, function(err, user) {
-        if(err)
-            res.send(err);
-        bcrypt.compare(req.body.password, user.password, function(err, hashRes) {
-                if(hashRes) {
-                 // Passwords match
-                 user.connectionID = req.body.connectionID;
-                 user.save();
-                 res.send(req.params.username);
-                } else {
-                 // Passwords don't match
-                 res.status(401).send();
-                } 
-              });
+  User.findOne({ username: req.params.username }, function(err, user) {
+    if (err) res.send(err);
+    bcrypt.compare(req.body.password, user.password, function(err, hashRes) {
+      if (hashRes) {
+        // Passwords match
+        user.connectionID = undefined;
+        user.save();
+        res.send(req.params.username);
+      } else {
+        // Passwords don't match
+        res.status(401).send();
+      }
     });
-}
-
-
+  });
+};
--- server.js
+++ server.js
@@ -1,28 +1,29 @@
-var express = require('express'),
+var express = require("express"),
   app = express(),
   port = process.env.PORT || 3000,
-  mongoose = require('mongoose'),
-  User = require('./api/models/userModel'), //created model loading here
-  bodyParser = require('body-parser');
-  
+  mongoose = require("mongoose"),
+  User = require("./api/models/userModel"), //created model loading here
+  bodyParser = require("body-parser");
+
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/userdb'); 
-
+mongoose.connect("mongodb://localhost/userdb");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
 
-var routes = require('./api/routes/userRoutes'); //importing route
+var routes = require("./api/routes/userRoutes"); //importing route
 routes(app); //register the route
-
 
 app.listen(port);
 
-
-console.log('todo list RESTful API server started on: ' + port);
+console.log("todo list RESTful API server started on: " + port);
